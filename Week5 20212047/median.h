f58808017446d7c343ef05c6073c87199d419efe
JONGHYUK02 <qkrwhdgur890@gmail.com>
2025-04-04 16:51:26 +0900

    week5 20212047 박종혁

---
+++ Week5 20212047/median.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef GAURD_median_h
+#define GAURD_median_h
+
+// median.h
+#include <vector>
+#include "Student_info.h"
+
+double median(std::vector<double>);
+
+bool fgrade(const Student_info&);
+
+#endif
